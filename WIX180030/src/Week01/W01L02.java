c7f7d69fe0f5501d03fe61a7cfa8295d79728d0e
Wic180030 <Wic180030@MIK117.fsktm.edu>
2018-09-21 11:52:22 +0800

    Print Star

--- WIX180030/src/Week01/W01L02.java
+++ WIX180030/src/Week01/W01L02.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package wix180030;
+package Week01;
 
 /**
  *
---
+++ WIX180030/src/Week01/W01L04.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Week01;
+
+/**
+ *
+ * @author Wic180030
+ */
+public class W01L04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { 
+        System.out.println("   --------------|--------------");
+        System.out.println("   |  Month      |       Amount|");
+        System.out.println("   |-------------|-------------|");
+        System.out.println("   | January 2016|         2500|");
+        System.out.println("   |-------------|-------------|");
+        System.out.println("   |February 2016|         1600|");
+        System.out.println("   |-------------|-------------|");
+        System.out.println("   | March 2016  |         2000|");
+        System.out.println("   |-------------|-------------|");
+        System.out.println("   | April 2016  |         2700|");
+        System.out.println("   |-------------|-------------|");
+        System.out.println("   |   May 2016  |         3200|");
+        System.out.println("   |-------------|-------------|");
+        System.out.println("   |  Jun 2016   |         800 |");
+        System.out.println("   |-------------|-------------|");
+    }
+    
+}
